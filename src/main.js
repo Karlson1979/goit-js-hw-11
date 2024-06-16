541f7c165f585d4a9c898c7af396db9763b5cf38
Tochka <tochka114@gmail.com>
2024-06-16 13:44:32 +0300

    iziToast

--- src/main.js
+++ src/main.js
@@ -8,29 +8,44 @@ import 'simplelightbox/dist/simple-lightbox.min.css';
 import { gallery } from './js/render-functions';
 import { getImg } from './js/pixabay-api';
 
-
 const formEl = document.querySelector('.form');
 const imgEl = document.querySelector('.nav-list');
 
-formEl.addEventListener('submit', e => {
-  e.preventDefault();
-  const value = e.target.elements.search.value.trim();
-  getImg(value)
+formEl
+  .addEventListener('submit', e => {
+    e.preventDefault();
+    const value = e.target.elements.search.value.trim();
+    if (!value) {
+      iziToast.error({
+        title: 'Error',
+        message: 'Please enter a search term',
+        position: 'topRight',
+      });
+      return;
+    }
+    getImg(value)
     .then(data => {
+      if (data.hits.length === 0) {
+        iziToast.warning({
+          title: 'No results',
+          message:
+            'Sorry, there are no images matching your search query. Please try again!',
+        });
+        return;
+      }
+
       const markup = gallery(data.hits);
       imgEl.innerHTML = markup;
       let lightbox = new SimpleLightbox('.gallery a');
-      lightbox.refresh()
-
-});
+      lightbox.refresh();
+      formEl.reset();
     })
-    .catch(error => console.error(error));
-
-
-
-
-
-
-
-
-
+    .catch(error => {
+      console.error(error);
+      iziToast.error({
+        title: 'Error',
+        message: 'Something went wrong. Please try again later.',
+        position: 'topRight',
+      });
+    });
+});
